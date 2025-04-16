683d2436743969c5ac637b11cf6f6773de3911aa
GabrielaTalaga <gabiki3600@gmail.com>
2025-04-16 10:41:02 +0200

    Add files via upload

---
+++ ćw-oddzielanie-logiki-aplikacji-od-wyglądu-2/docs/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\find_all\u0028\u0029",
+            "name": "find_all",
+            "summary": "Find\u0020all\u0020records.",
+            "url": "namespaces/default.html#function_find_all"
+        },                {
+            "fqsen": "\\find_one_by_id\u0028\u0029",
+            "name": "find_one_by_id",
+            "summary": "",
+            "url": "namespaces/default.html#function_find_one_by_id"
+        },                {
+            "fqsen": "\\render\u0028\u0029",
+            "name": "render",
+            "summary": "Render\u0020view.",
+            "url": "namespaces/default.html#function_render"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
